94cd983810259d712f86626508f0d9c6e0f65c36
James <james_baster@shelter.org.uk>
2016-02-24 15:39:24 +0000

    When you create a new project, make 2 basic field definitions

--- src/CaseStoreBundle/Controller/ProjectNewController.php
+++ src/CaseStoreBundle/Controller/ProjectNewController.php
@@ -2,6 +2,7 @@
 
 namespace CaseStoreBundle\Controller;
 
+use CaseStoreBundle\Entity\CaseStudyFieldDefinition;
 use CaseStoreBundle\Entity\Project;
 use CaseStoreBundle\Form\Type\ProjectNewType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -26,6 +27,25 @@ class ProjectNewController extends Controller
             $form->handleRequest($request);
             if ($form->isValid()) {
                 $doctrine->persist($project);
+
+                $fieldDefTitle = new CaseStudyFieldDefinition();
+                $fieldDefTitle->setProject($project);
+                $fieldDefTitle->setAddedBy($this->getUser());
+                $fieldDefTitle->setSort(0);
+                $fieldDefTitle->setType('string');
+                $fieldDefTitle->setTitle('Title');
+                $fieldDefTitle->setPublicId('title');
+                $doctrine->persist($fieldDefTitle);
+
+                $fieldDefDescription = new CaseStudyFieldDefinition();
+                $fieldDefDescription->setProject($project);
+                $fieldDefDescription->setAddedBy($this->getUser());
+                $fieldDefDescription->setSort(1);
+                $fieldDefDescription->setType('text');
+                $fieldDefDescription->setTitle('Description');
+                $fieldDefDescription->setPublicId('description');
+                $doctrine->persist($fieldDefDescription);
+
                 $doctrine->flush();
                 return $this->redirect($this->generateUrl('case_store_project', array('projectId'=>$project->getPublicId())));
             }
