e423634e974c9b6d38546c369095da4f7f1ffbd6
James <james_baster@shelter.org.uk>
2016-03-09 16:41:45 +0000

    More tests on ProjectVoter

---
+++ src/CaseStoreBundle/Tests/Security/ProjectVoterTest.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace CaseStoreBundle\Tests\Security;
+
+
+
+use CaseStoreBundle\Entity\Project;
+use CaseStoreBundle\Entity\User;
+use CaseStoreBundle\Security\ProjectVoter;
+use CaseStoreBundle\Tests\BaseTestWithDataBase;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+
+/**
+ *  @license 3-clause BSD
+ *  @link https://github.com/CaseStore/CaseStore-Core
+ */
+class ProjectVoterTest extends BaseTestWithDataBase {
+
+
+    public function testViewByOwner() {
+
+
+        // Build Data
+        $user = new User();
+        $user->setEmail("test@example.com");
+        $user->setUsername("test");
+        $user->setPassword("ouhosu");
+        $this->em->persist($user);
+
+        $project = new Project();
+        $project->setTitle('Test');
+        $project->setPublicId('test');
+        $project->setOwner($user);
+        $this->em->persist($project);
+
+        $this->em->flush();
+
+        // Test!
+        $projectVoter = new ProjectVoter();
+
+        $token = new TokenInterfaceForTesting();
+        $token->setUser($user);
+
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED , $projectVoter->vote($token, $project, array(ProjectVoter::VIEW)));
+
+    }
+
+
+    public function testViewByAnonymous() {
+
+
+        // Build Data
+        $user = new User();
+        $user->setEmail("test@example.com");
+        $user->setUsername("test");
+        $user->setPassword("ouhosu");
+        $this->em->persist($user);
+
+        $project = new Project();
+        $project->setTitle('Test');
+        $project->setPublicId('test');
+        $project->setOwner($user);
+        $this->em->persist($project);
+
+        $this->em->flush();
+
+        // Test!
+        $projectVoter = new ProjectVoter();
+
+        $token = new TokenInterfaceForTesting();
+        $token->setUser(null);
+
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED , $projectVoter->vote($token, $project, array(ProjectVoter::VIEW)));
+
+    }
+
+    public function testViewByOther() {
+
+
+        // Build Data
+        $user = new User();
+        $user->setEmail("test@example.com");
+        $user->setUsername("test");
+        $user->setPassword("ouhosu");
+        $this->em->persist($user);
+
+        $project = new Project();
+        $project->setTitle('Test');
+        $project->setPublicId('test');
+        $project->setOwner($user);
+        $this->em->persist($project);
+
+
+        $userOther = new User();
+        $userOther->setEmail("testOther@example.com");
+        $userOther->setUsername("testOther");
+        $userOther->setPassword("ouhosu");
+        $this->em->persist($userOther);
+
+        $this->em->flush();
+
+        // Test!
+        $projectVoter = new ProjectVoter();
+
+        $token = new TokenInterfaceForTesting();
+        $token->setUser($userOther);
+
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED , $projectVoter->vote($token, $project, array(ProjectVoter::VIEW)));
+
+    }
+
+
+    public function testAdminByOwner() {
+
+
+        // Build Data
+        $user = new User();
+        $user->setEmail("test@example.com");
+        $user->setUsername("test");
+        $user->setPassword("ouhosu");
+        $this->em->persist($user);
+
+        $project = new Project();
+        $project->setTitle('Test');
+        $project->setPublicId('test');
+        $project->setOwner($user);
+        $this->em->persist($project);
+
+        $this->em->flush();
+
+        // Test!
+        $projectVoter = new ProjectVoter();
+
+        $token = new TokenInterfaceForTesting();
+        $token->setUser($user);
+
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED , $projectVoter->vote($token, $project, array(ProjectVoter::ADMIN)));
+
+    }
+
+
+    public function testAdminByAnonymous() {
+
+
+        // Build Data
+        $user = new User();
+        $user->setEmail("test@example.com");
+        $user->setUsername("test");
+        $user->setPassword("ouhosu");
+        $this->em->persist($user);
+
+        $project = new Project();
+        $project->setTitle('Test');
+        $project->setPublicId('test');
+        $project->setOwner($user);
+        $this->em->persist($project);
+
+        $this->em->flush();
+
+        // Test!
+        $projectVoter = new ProjectVoter();
+
+        $token = new TokenInterfaceForTesting();
+        $token->setUser(null);
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED , $projectVoter->vote($token, $project, array(ProjectVoter::ADMIN)));
+
+    }
+
+    public function testAdminByOther() {
+
+
+        // Build Data
+        $user = new User();
+        $user->setEmail("test@example.com");
+        $user->setUsername("test");
+        $user->setPassword("ouhosu");
+        $this->em->persist($user);
+
+        $project = new Project();
+        $project->setTitle('Test');
+        $project->setPublicId('test');
+        $project->setOwner($user);
+        $this->em->persist($project);
+
+
+        $userOther = new User();
+        $userOther->setEmail("testOther@example.com");
+        $userOther->setUsername("testOther");
+        $userOther->setPassword("ouhosu");
+        $this->em->persist($userOther);
+
+        $this->em->flush();
+
+        // Test!
+        $projectVoter = new ProjectVoter();
+
+        $token = new TokenInterfaceForTesting();
+        $token->setUser($userOther);
+
+        $this->assertEquals(VoterInterface::ACCESS_DENIED , $projectVoter->vote($token, $project, array(ProjectVoter::ADMIN)));
+
+    }
+
+
+
+}
+
